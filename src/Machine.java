f6a0d60432c1309b09b9c6b8402f4df3b3619b7e
Horacio Valdes <horavaldesg@gmail.com>
2022-03-27 00:34:49 -0400

    Is Full not Completed

--- src/Machine.java
+++ src/Machine.java
@@ -1,4 +1,12 @@
 
 public class Machine {
 
+    // Get washer/Dryer full data
+    public static boolean IsFull() {
+        // if(washer/dryer.hasClothes)
+        return true;
+        // else
+        // return false
+    }
+
 }
