daa74bce4647b3cb2001f142de69bd7cec81d960
Horacio Valdes <horavaldesg@gmail.com>
2022-03-28 11:59:40 -0400

    Random to display time remaining

--- src/Machine.java
+++ src/Machine.java
@@ -3,7 +3,11 @@ public class Machine {
 
     // Get washing/dryer and get time remaining
     public static float TimeRemaining() {
-        // float timer remaining
-        return 0;
+        // Returns a random value between 0 and 60 which account for minutes remaining
+        float minNumber = 0;
+        float maxNumber = 60;
+        float timeRemaining = (float) Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+
+        return timeRemaining;
     }
 }
