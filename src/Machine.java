e8eccbf000388991e6a84c1a68239c3b23401562
Horacio Valdes <horavaldesg@gmail.com>
2022-03-28 12:04:29 -0400

    Is full random value

--- src/Machine.java
+++ src/Machine.java
@@ -1,12 +1,25 @@
 
 public class Machine {
+    public static void main(String[] args) {
+        System.out.println((IsFull()));
+    }
 
     // Get washer/Dryer full data
     public static boolean IsFull() {
-        // if(washer/dryer.hasClothes)
-        return true;
-        // else
-        // return false
+        // Random values for now
+        int falseInt = 0;
+        int trueInt = 1;
+
+        int boolValue = (int) Math.floor(Math.random() * (trueInt - falseInt + 1) + falseInt);
+        // If dryer doesn't have clothes
+        if (boolValue == 0) {
+            return false;
+        }
+        // If dryer has clothes
+        else {
+            return true;
+        }
+
     }
 
 }
