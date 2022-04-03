99aca131257bb9b283f80f3afee2ef62b01054e6
dwidecki <dylan.widecki@quinnipiac.edu>
2022-04-03 16:13:22 -0400

    Empty Laundry App interface now displays for user

--- src/MachineApp.java
+++ src/MachineApp.java
@@ -1,8 +1,26 @@
+import java.awt.BorderLayout;
+import java.awt.Rectangle;
 
-public class MachineApp {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MachineApp extends JFrame {
+	
+	public MachineApp() {
+		super("Laundry App!");
+		this.setSize(500, 600);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		
+		this.add(mainPanel);
+		this.setVisible(true);
+		
+		
+	}
 
 	public static void main(String[] args) {
-		
+		new MachineApp();
 	}
 	
 }
